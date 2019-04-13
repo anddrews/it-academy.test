ae9ead10f4047526b9e85e51f16e116c0199e7cb
naumenko <naumenkosergeyns@gmail.com>
2019-04-13 15:16:04 +0300

    js-start

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,25 @@
-function test() {
-    var eng = ['green', 'yellow', 'red', 'black'];
-    var rus = ['зеленый', 'желтый', 'красный', 'черный'];
+var questions = ['First name', 'Last name', 'Gender', 'Age'];
+var answers = [];
 
-    do {
-        var index = Math.ceil(Math.random() * (eng.length - 1));
-        var answer = prompt(eng[index]);
-        if(rus[index] == answer) {
-            alert('You are cool guy!')
-        } else {
-            alert('Go to scoll!')
-        }
-    } while (confirm('continue?'));
+// for (var i = 0; i < questions.length; i++) {
+//     prompt('Введите ' + questions[i]);
+// }
+
+// alert(answers);
+
+function Fuctorial(num) {
+    if (num > 1) {
+        return num * Fuctorial(num - 1);
+    } else {
+        return 1;
+    }
 }
 
-function test1() {
-    var dictionary = {
-        green: 'зеленый',
-        yellow: 'желтый',
-        red: 'красный',
-        black: 'черный'
-    };
-
-    do {
-        var index = Math.ceil(Math.random() * (eng.length - 1));
-        var quest = Object.keys(dictionary)[index];
-        var answer = prompt(quest);
-        if(dictionary[quest] == answer) {
-            alert('You are cool guy!')
-        } else {
-            alert('Go to scoll!')
-        }
-    } while (confirm('continue?'));
+var result = 1;
+for (var i = 1; i <= 3; i++) {
+    result = result * i;
 }
 
-test1();
+console.log(result);
+
+console.log(Fuctorial(3));
