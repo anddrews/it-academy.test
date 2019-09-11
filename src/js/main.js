839d08921161d1f92d3f494db04e89a37a300cda
anddrews <anddrews@tut.by>
2019-09-11 21:18:52 +0300

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,14 @@
-var arr = [];
+var wrapp = document.querySelector('.numbers');
+var res = document.querySelector('.result');
 
-function sort(arr) {
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = i + 1; j < arr.length; j ++) {
-            if (arr[i] > arr[j]) {
-                var tmp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tmp;
-            }
-        }
+wrapp.addEventListener('click', function (e) {
+    var id = e.target.id
+    
+    if(parseInt(id)) {
+        res.innerText = res.innerText ? res.innerText + id : id;
     }
 
-    return arr;
-}
-
-function main() {
-    do {
-        var value = prompt('Input number');
-        if(value) {
-            arr.push(value);
-        }
-    } while (value);
-
-    alert(sort(arr));  
-    alert('min ' + arr[0])
-    alert('max ' + arr[arr.length - 1])
-};
-
-document.querySelector('.button-map').addEventListener('click', main);
-
-var wrap = document.querySelector('.preview');
-var prev = document.querySelector('.img__preview--small');
-
-prev.addEventListener('click', () => wrap.classList.toggle('active'));
-
-var prev = document.querySelector('.img__preview');
-
-prev.addEventListener('click', () => wrap.classList.toggle('active'));
+    if (id === 'call') {
+        res.innerText = ''
+    }
+})
